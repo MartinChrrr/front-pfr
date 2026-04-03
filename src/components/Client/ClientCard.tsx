c3ecb0a59e0917970233809dd703b431cd94030c
Phosnay <nils.romano@gmail.com>
2026-04-03 15:12:37 +0200

    Change to the font + minor visual modifications

--- src/components/Client/ClientCard.tsx
+++ src/components/Client/ClientCard.tsx
@@ -9,20 +9,20 @@ type ClientCardProps = {
 export default function ClientCard({ client, className }: ClientCardProps) {
     return (
     <Card title="Infomation du clients" className={className}>
-        <p>Entreprise :</p>
-        <p> {client.raison_sociale}</p>
-        <p> Contact :</p>
-        <p> {client.contact_nom}</p>
-        <p> Email :</p>
-        <p> {client.contact_email}</p>
-        <p> Téléphone : </p>
-        <p> {client.contact_telephone}</p>
-        <p> Adresse :</p>
-        <p> {client.adresses[0]?.ligne1} {client.adresses[0]?.code_postal} {client.adresses[0]?.ville} </p>
-        <p> Siret :</p>
-        <p> {client.siret}</p>
-        <p> Client depuis :</p>
-        <p>
+        <p className="font-medium">Entreprise :</p>
+        <p className="px-2"> {client.raison_sociale ? client.raison_sociale : "Non renseigné"}</p>
+        <p className="font-medium"> Contact :</p>
+        <p className="px-2"> {client.contact_nom ? client.contact_nom : "Non renseigné"}</p>
+        <p className="font-medium"> Email :</p>
+        <p className="px-2"> {client.contact_email ? client.contact_email : "Non renseigné"}</p>
+        <p className="font-medium"> Téléphone : </p>
+        <p className="px-2"> {client.contact_telephone ? client.contact_telephone : "Non renseigné"}</p>
+        <p className="font-medium"> Adresse :</p>
+        <p className="px-2"> {client.adresses[0]?.ligne1} {client.adresses[0]?.code_postal} {client.adresses[0]?.ville} </p>
+        <p className="font-medium"> Siret :</p>
+        <p className="px-2"> {client.siret ? client.siret : "Non renseigné"}</p>
+        <p className="font-medium"> Client depuis :</p>
+        <p className="px-2">
           {
             // Converts raw text "2026-04-02T10:30:00" to a JS date object
             new Date(client.created_at)
--- src/components/Quotes/LittleClientsCard.tsx
+++ src/components/Quotes/LittleClientsCard.tsx
@@ -9,18 +9,18 @@ type LittleClientsCardProps = {
 export default function LittleClientsCard({ client, className }: LittleClientsCardProps) {
     return (
         <Card title="Clients" className={`${className}`} >
-            <p>Entreprise</p>
-            <p>{client.raison_sociale}</p>
-            <p>Email</p>
-            <p>{client.email}</p>
-            <p>Téléphone</p>
-            <p>{client.telephone}</p>
-            <p>Adresse</p>
-            <p>
+            <p className="font-medium">Entreprise :</p>
+            <p className="px-2">{client.raison_sociale}</p>
+            <p className="font-medium">Email :</p>
+            <p className="px-2">{client.email}</p>
+            <p className="font-medium">Téléphone :</p>
+            <p className="px-2">{client.telephone}</p>
+            <p className="font-medium">Adresse :</p>
+            <p className="px-2">
                 {client.adresses[0].ligne1} {client.adresses[0].code_postal} {client.adresses[0].ville}
             </p>
-            <p>Siret</p>
-            <p>{client.siret}</p>
+            <p className="font-medium">Siret :</p>
+            <p className="px-2">{client.siret}</p>
         </Card>
     )
 }
