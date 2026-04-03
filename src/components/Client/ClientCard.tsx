3573a2bc61a60edad6e4013404abc4fb0735448d
Phosnay <nils.romano@gmail.com>
2026-04-03 14:30:04 +0200

    Added ":" to clientcard

--- src/components/Client/ClientCard.tsx
+++ src/components/Client/ClientCard.tsx
@@ -9,19 +9,19 @@ type ClientCardProps = {
 export default function ClientCard({ client, className }: ClientCardProps) {
     return (
     <Card title="Infomation du clients" className={className}>
-        <p>Entreprise</p>
+        <p>Entreprise :</p>
         <p> {client.raison_sociale}</p>
-        <p> Contact</p>
+        <p> Contact :</p>
         <p> {client.contact_nom}</p>
-        <p> Email</p>
+        <p> Email :</p>
         <p> {client.contact_email}</p>
-        <p> Téléphone</p>
+        <p> Téléphone : </p>
         <p> {client.contact_telephone}</p>
-        <p> Adresse</p>
+        <p> Adresse :</p>
         <p> {client.adresses[0]?.ligne1} {client.adresses[0]?.code_postal} {client.adresses[0]?.ville} </p>
-        <p> Siret</p>
+        <p> Siret :</p>
         <p> {client.siret}</p>
-        <p>Client depuis</p>
+        <p>Client depuis :</p>
         <p> {client.created_at}</p>
     </Card>
     );
